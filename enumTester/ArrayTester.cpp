40b6d6635003f867224cbfa11d97666c74e11a36
Jork <r.francis.yeet@outlook.com>
2026-09-10 16:44:24 -0700

    array type shi

---
+++ enumTester/ArrayTester.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <format>
+//#include <string>
+//#include <cctype>
+//using namespace std;
+//int main()
+//{
+//	int intCount = 0;
+//	int scores[5] = {10, 87, 92, 78, 50};
+//	for (int i = 0; i < 5; i++) { cout << scores[i] << ","; intCount++; }
+//	cout << endl << intCount << endl;
+//	int p2[4] = { 5, 10, 15, 20 };
+//	cout << p2[1];
+//
+//}
